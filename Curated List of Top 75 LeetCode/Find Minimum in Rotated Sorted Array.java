ec68f010fdec8b17804bcf1c3871f81e4f0d9142
YeongJin Kim <66052467+dudwls901@users.noreply.github.com>
2025-06-18 21:31:18 +0900

    20250618

--- Curated List of Top 75 LeetCode/Find Minimum in Rotated Sorted Array.java
+++ Curated List of Top 75 LeetCode/Find Minimum in Rotated Sorted Array.java
@@ -5,12 +5,13 @@ class Solution {
         int e = nums.length - 1;
         int answer = Integer.MAX_VALUE;
         while (s <= e) {
-            int left = (s + e) / 2;
-            if (nums[left] > nums[e])
-                s = left + 1;
-            else
-                e = left - 1;
-            answer = Math.min(answer, nums[left]);
+            int mid = (s + e) / 2;
+            if(nums[mid] < nums[e]){
+                e = mid - 1;
+            }else{
+                s = mid + 1;
+            }
+            answer = Math.min(answer, nums[mid]);
         }
         return answer;
     }
